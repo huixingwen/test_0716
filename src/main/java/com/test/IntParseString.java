7ce21489a84735f9a3345327f6895199e3c4c63a
wenxinghui <wenxinghui@feiniu.com>
2020-07-16 14:43:50 +0800

    提交

---
+++ src/main/java/com/test/IntParseString.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * @Author wenxinghui
+ * @Date 2020/6/17 16:39
+ * @Description TODO
+ */
+public class IntParseString {
+
+    public static void main(String[] args) {
+        System.out.println(Double.parseDouble("99.99"));
+    }
+}
