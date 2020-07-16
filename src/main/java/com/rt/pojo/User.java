adf985c3eae37968167d1e82d3a83d083a1c1b8a
wenxinghui <wenxinghui@feiniu.com>
2020-07-16 14:45:00 +0800

    提交

---
+++ src/main/java/com/rt/pojo/User.java
@@ -0,0 +1,19 @@
+package com.rt.pojo;
+
+/**
+ * @Date 2020/7/16 14:44
+ * @Created by wenxinghui
+ * @Description TODO
+ */
+public class User {
+
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
